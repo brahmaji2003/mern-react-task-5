84e1bc6b938009f749a254cdb27d28ee8c3449bf
brahmaji2003 <srisailapubrahmaji@gmail.com>
2023-10-31 21:49:01 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import AgeCalculator from './components/agecalc'
+
+function App() {
+  return (
+    <div className="App">
+      <AgeCalculator />
+    </div>
+  );
+}
+
+export default App;
